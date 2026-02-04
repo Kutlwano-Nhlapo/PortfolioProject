e855a4e8a4e94e62cc619a22bc1715d2c30cd795
Kutlwano Nhlapo <Kutlwanonhlapo@gmail.com>
2026-02-04 14:53:08 +0200

    vite fix

--- portfolio-project/vite.config.js
+++ portfolio-project/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-project/",
+  base: "/PortfolioProject/",
   // Note: Replace 'portfolio' with your actual GitHub repository name
 })
