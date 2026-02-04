cfb2513964380491f82c138c1848129ee32a7e5e
Kutlwano Nhlapo <Kutlwanonhlapo@gmail.com>
2026-02-04 13:55:02 +0200

    changed the vite.js

--- portfolio-project/vite.config.js
+++ portfolio-project/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/PortfolioProject/' : '/',
+  base: "/portfolio-project/",
   // Note: Replace 'portfolio' with your actual GitHub repository name
 })
